542da0ce480ff32ff39d1277dcda4881b014e997
Benjamin Zulauf <benjaminzulauf@Benjamins-MacBook-Pro.local>
2017-08-03 16:38:29 -0700

    commit 2

--- public/main.js
+++ public/main.js
@@ -1,8 +1,15 @@
+function writeUserData(name, telephone) {
+    firebase.database().ref('users/' + telephone).set({
+        username: name,
+        telephone: telephone
+    });
+}
+
 function handleSignup(evt) {
     evt.preventDefault();
     const data = new FormData(evt.target);
 
-    firebase.database().ref().child("users").update
+    writeUserData(data.get("name"), data.get("telephone"));
 }
 
 document.getElementById("signup").addEventListener("submit", handleSignup, false);
